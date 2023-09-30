6164711482caeb9e55500e932d2763ba301af7c2
Zunnurhaq Zulkifli <zunnurhaq123@gmail.com>
2023-10-01 02:40:28 +0800

    failed to rename revert to original filename on Author model

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
  */
-class AuthorsFactory extends Factory
+class AuthorFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,7 @@ class AuthorsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
         ];
     }
 }
--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AuthorsSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Author::factory(5)->create();
     }
 }
